29cca8a1aa1c85b0bf1cdad355384e8004f2154e
palmmanee <palmmanee28@hotmail.com>
2020-09-22 18:48:10 +0700

    MVC-Controller

--- mvc/mvc/controllers/main_controller.py
+++ mvc/mvc/controllers/main_controller.py
@@ -4,19 +4,23 @@ class MainController:
     def __init__(self):
         # Create note repository here
         # Your code here
+        self.np = NoteRepository()
         pass
     
     def get_all_notes(self):
         # Return all notes
         # Your code here
+        return self.np.get_all_notes()
         pass
 
     def add_note(self, note: str):
         # Add note
         # Your code here
+        self.np.add_note(note)
         pass
 
     def clear_all(self):
         # Clear all note
         # Your code here
+        self.np.clear_all_notes()
         pass
